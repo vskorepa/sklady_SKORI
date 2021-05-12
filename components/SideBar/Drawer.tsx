dedd006b85a3499518adde6bc2e3d4dd27b53893
vskorepa <vskorepa@skori.cz>
2021-05-12 10:59:34 +0200

    funkcni sideNav + TopNav

---
+++ components/SideBar/Drawer.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles } from "@material-ui/core";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { ListLinks } from "./NavList";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+  },
+
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9),
+    },
+  },
+}));
+
+export const SideBar: React.FC = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Drawer
+      anchor="left"
+      variant="permanent"
+      classes={{
+        paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+      }}
+      open={open}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <ListLinks />
+    </Drawer>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "../components/Header/TopNav";
+
+function App() {
+  return <NavBar></NavBar>;
+}
+export default App;
